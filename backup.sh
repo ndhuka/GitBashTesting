4a8a9e12abe766dbed56218f97c1adf643bab55c
ndhuka <66150274+ndhuka@users.noreply.github.com>
2020-10-04 17:04:12 -0500

    Update backup.sh
    change the parameter for the if statement to match my dir

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ function backup {
     if [ -z $1 ]; then
     	user=$(whoami)
     else
-    	if [ ! -d "/home/$1" ]; then
+    	if [ ! -d "c/Users/$1" ]; then
     		echo "Requested $1 user home directory doesn't exist."
     		exit 1
     	fi
