9e579f7a4cebc3c1b8950571ab812c93612c09f9
Karansanghera <71192483+Karansanghera@users.noreply.github.com>
2020-10-31 02:24:35 -0500

    Update ifsandloops.sh

--- ifsandloops.sh
+++ ifsandloops.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# this is the block for the if statement
+# This is the block for the if statement
+# The purpose of the if-else block is that if the first specified condition is true, the code gets executed. 
+# If the first condition is false, "else" executes a new condition. 
+# An interesting feature about the if-else block is that it returns a boolean value (true and false).
 
 num_a=400
 num_b=200
@@ -13,7 +16,9 @@ fi
 
 
 
-# this is the block for the for loop
+# This is the block for the for loop
+# The purpose of the for loop is that it executes the code multiple times at once.
+# An interesting feature about the for loop is that each time the code executes, it is able to run using a different value each time.
 
 for i in 1 2 3; do
     echo $i
@@ -21,7 +26,10 @@ done
 
 
 
-# this is the block for the while loop
+# This is the block for the while loop
+# The purpose of the while loop is that it runs the code multiple times as long as the condition given stays true.
+# An interesting feature about the while loop is that the code runs as long as the boolean condition is true similar to 
+# the if-else loop but also has the ability to run multiple times similar to the for loop.
 
 counter=0
 while [ $counter -lt 3 ]; do
@@ -31,7 +39,9 @@ done
 
 
 
-# this is the block for the until loop
+# This is the block for the until loop
+# The purpose of the until loop is that it runs the code repeatedly until the command executes successfully.
+# An interesting feature is that that the variable counter holds the current value each time the code runs. 
 
 counter=6
 until [ $counter -lt 3 ]; do
